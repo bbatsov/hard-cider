7a9e2d5a6170e5584f3d95c6c2aaf8e28e55f564
Bozhidar Batsov <bozhidar@batsov.com>
2019-05-15 11:14:14 +0300

    Initial commit

---
+++ src/hard_cider/core.clj
@@ -0,0 +1,6 @@
+(ns hard-cider.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/hard_cider/core_test.clj
@@ -0,0 +1,7 @@
+(ns hard-cider.core-test
+  (:require [clojure.test :refer :all]
+            [hard-cider.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
